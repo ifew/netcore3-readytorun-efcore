abd60570a263594b67a3bb45661a3061bb81edc8
iFew <chitpong@gmail.com>
2019-09-28 21:39:15 +0700

    Fixed build script

--- src/aws-lambda-netcore3-readytorun-efcore/build_linux.sh
+++ src/aws-lambda-netcore3-readytorun-efcore/build_linux.sh
@@ -9,5 +9,6 @@ rm -f $(pwd)/bootstrap
 rm -f $(pwd)/package.zip
 docker run -it --rm -v $(pwd):/app lambdareadytorun:3.0
 cp bin/Release/netcoreapp3.0/linux-x64/publish/aws-lambda-netcore3-readytorun-efcore bootstrap
+chmod 777 bootstrap
 zip package.zip bootstrap
-aws s3 cp package.zip s3://backend-layer --profile MyRole
+aws s3 cp package.zip s3://ifewtemp
