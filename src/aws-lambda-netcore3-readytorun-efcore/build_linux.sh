fb03054090c1a63b00b5b59fb4bfdcb555ac9342
iFew <chitpong@gmail.com>
2019-09-28 21:10:02 +0700

    Initial Project

---
+++ src/aws-lambda-netcore3-readytorun-efcore/build_linux.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+#
+# SHOULD BUILD IMAGE BEFORE
+# docker build -t lambdanative:2.2 .
+#
+
+rm -f $(pwd)/bootstrap
+rm -f $(pwd)/package.zip
+docker run -it --rm -v $(pwd):/app lambdareadytorun:3.0
+cp bin/Release/netcoreapp3.0/linux-x64/publish/aws-lambda-netcore3-readytorun-efcore bootstrap
+zip package.zip bootstrap
+aws s3 cp package.zip s3://backend-layer --profile MyRole
